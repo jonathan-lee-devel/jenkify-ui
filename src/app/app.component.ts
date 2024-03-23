80a9eea64bb2e4c2d05d196e21d27f6cf47cd2d7
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-03-23 14:00:14 +0000

    Update Angular dependencies, integrate ESLint and update package-lock.json
    This update includes a comprehensive set of changes. The main tasks performed include an upgrade of several Angular dependencies to version 17.3.1. Additionally, new dependencies relating to ESLint were introduced; this includes various Angular-ESLint and TypeScript-ESLint plugins. The package-lock.json file was subsequently updated to reflect these changes. This upgrade aims to enhance the overall code quality, enable efficient linting, and maintain the updated state of the application's dependencies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
-import {NavbarComponent} from "./components/lib/navbar/navbar.component";
-import {Store} from "@ngrx/store";
-import {UserAuthenticationActions, UserAuthenticationSelector} from "./+state";
-import {rebaseRoutePath, rebaseRoutePathAsString, RoutePath} from "./app.routes";
-import {SidebarService} from "./services/sidebar/sidebar.service";
-import {Subscription, tap} from "rxjs";
-import {MatButton} from "@angular/material/button";
-import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
-import {SidebarComponent} from "./components/lib/sidebar/sidebar.component";
+import {MatButton} from '@angular/material/button';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {Subscription, tap} from 'rxjs';
+
+import {UserAuthenticationActions, UserAuthenticationSelector} from './+state';
+import {rebaseRoutePath, rebaseRoutePathAsString, RoutePath} from './app.routes';
+import {NavbarComponent} from './components/lib/navbar/navbar.component';
+import {SidebarComponent} from './components/lib/sidebar/sidebar.component';
+import {SidebarService} from './services/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,7 @@ import {SidebarComponent} from "./components/lib/sidebar/sidebar.component";
     SidebarComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'jenkify-ui';
@@ -38,21 +39,23 @@ export class AppComponent implements OnInit, OnDestroy {
     private readonly sidebarService: SidebarService,
   ) {
     this.isSidebarOpenedSubscription = this.sidebarService.isSideBarOpened$
-      .pipe(
-        tap((isSideBarOpened) => {
-          this.opened = isSideBarOpened;
-        }),
-      ).subscribe();
-    this.isLoggedInSubscription = this.store.select(UserAuthenticationSelector.selectLoggedInState()).pipe(
-      tap((loggedInState) => {
-        this.isLoggedIn = loggedInState === 'LOGGED_IN';
-      }),
-    ).subscribe();
+        .pipe(
+            tap((isSideBarOpened) => {
+              this.opened = isSideBarOpened;
+            }),
+        ).subscribe();
+
+    this.isLoggedInSubscription = this.store.select(UserAuthenticationSelector.selectLoggedInState())
+        .pipe(
+            tap((loggedInState) => {
+              this.isLoggedIn = loggedInState === 'LOGGED_IN';
+            }),
+        ).subscribe();
   }
 
   ngOnInit() {
     this.store.dispatch(UserAuthenticationActions.checkLoginOnRefresh({
-      next: encodeURIComponent(rebaseRoutePathAsString(RoutePath.HOME_PAGE))
+      next: encodeURIComponent(rebaseRoutePathAsString(RoutePath.HOME_PAGE)),
     }));
   }
 
